19b15cc74443e1cacd5c2b76bcc1a847326349a7
lit@pencho.com.cn <lit@pencho.com.cn>
2016-03-14 20:45:37 +0800

    同步操作 需要加异步处理
    !!! !!!!同步操作 需要加异步处理
    
          NSAttributedString * attrStr = [[NSAttributedString alloc]
    initWithData:[htmlString dataUsingEncoding:NSUnicodeStringEncoding]
    options:@{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType }
    documentAttributes:nil error:nil];

--- HtmlLabel/ViewController.m
+++ HtmlLabel/ViewController.m
@@ -17,6 +17,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    /*
+     !!! !!!!同步操作 需要加异步处理
+
+      NSAttributedString * attrStr = [[NSAttributedString alloc] initWithData:[htmlString dataUsingEncoding:NSUnicodeStringEncoding] options:@{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType } documentAttributes:nil error:nil];
+     */
+    UILabel *lab = [[UILabel alloc] initWithFrame:CGRectMake(10, 100, 300, 20)];
+    [self.view addSubview:lab];
+
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        NSString * htmlString = @"<div style='background-color:#F1F1F1; font-size:14px; color:#304182;text-align:center; margin-left:10px; padding-right:10px'><p>iOS<span style='font-size:18px; color:#E88834;'>Developer</span>Tips</p></div>";
+        NSAttributedString * attrStr = [[NSAttributedString alloc] initWithData:[htmlString dataUsingEncoding:NSUnicodeStringEncoding] options:@{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType } documentAttributes:nil error:nil];
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            lab.attributedText = attrStr;
+
+        });
+        
+    });
+    
 }
 
 - (void)didReceiveMemoryWarning {
